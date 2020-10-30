17bd7c706d830169b96c563aa390c0493cb4bf8f
theart84 <artshadow74@gmail.com>
2020-10-30 21:34:03 +0500

    Исправил loginPage.js и login.html. Button по-умолчанию перезагружал страницу, в любом случае.

--- public/loginPage.js
+++ public/loginPage.js
@@ -2,18 +2,22 @@
 
 const newUser = new UserForm();
 
-newUser.loginFormCallback = data => {
-  ApiConnector.login(data, (response) => {
-    if (!response.success)
-      newUser.setLoginErrorMessage(response.error);
+newUser.loginFormCallback = data => ApiConnector.login(data, checkLogin);
+
+function checkLogin(response) {
+  if (response.success) {
     location.reload();
-  })
+  } else {
+    newUser.setLoginErrorMessage(response.error);
+  }
 }
 
-newUser.registerFormCallback = data => {
-  ApiConnector.register(data, (response) => {
-    if (!response.success)
-      newUser.setRegisterErrorMessage(response.error);
+newUser.registerFormCallback = data => ApiConnector.register(data, checkRegistry);
+
+function checkRegistry(response) {
+  if (response.success) {
     location.reload();
-  })
+  } else {
+    newUser.setRegisterErrorMessage(response.error);
+  }
 }
